961a3495bc20fcadb28465f6b7b73df1c572cb76
Lynn <dazzlynnnn@gmail.com>
2021-03-10 00:49:27 +0900

    search API
    parsing fail

--- locord/MapScreen/mapView.swift
+++ locord/MapScreen/mapView.swift
@@ -7,10 +7,22 @@
 
 import UIKit
 import NMapsMap
+import SwiftyJSON
+import Alamofire
+import Foundation
+
 class mapView: UIViewController {
     
+    struct Location: Codable{
+        let display : Int
+        let mapy : Int
+    }
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    let jsconDecoder: JSONDecoder = JSONDecoder()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         let x = view.center.x - 190
         let y = view.center.y - 190
@@ -34,5 +46,31 @@ class mapView: UIViewController {
             marker.mapView = mapView
         
     }
-}
+    
+    @IBAction func touchUpSearchButton(_ sender: Any) {
+        let queryValue: String = searchTextField.text!
+        requestAPIToNaver(queryValue: queryValue)
+    }
 
+    
+    func requestAPIToNaver(queryValue: String) {
+        let headers: HTTPHeaders=[
+            "X-Naver-Client-Id":"woibqApXFlygrGKGO5nZ",
+            "X-Naver-Client-Secret":"bdF8QljG3z",
+            "Content-Type":"application/json; charset=utf-8"
+        ]
+        let url = "https://openapi.naver.com/v1/search/local.json?query=\(queryValue)"
+        let encodedURL = url.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+        let queryURL: URL = URL(string: encodedURL)!
+        
+        
+        AF.request(queryURL,
+                   method: .get,
+                   parameters: nil,
+                   encoding: URLEncoding.default,
+                   headers: headers).responseData{ response in
+                    
+                        debugPrint(response)
+                   }
+}
+}
---
+++ locord/locord/location.swift
@@ -0,0 +1,8 @@
+//
+//  location.swift
+//  locord
+//
+//  Created by 이해린 on 2021/03/09.
+//
+
+import Foundation
